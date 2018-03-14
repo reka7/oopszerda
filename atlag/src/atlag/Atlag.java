66c266f815797967d5bc9d420bfbb5e119cfafe0
rasi <rasi@iit434.iit.uni-miskolc.hu>
2018-03-14 13:43:23 +0100

    Átlag

---
+++ atlag/src/atlag/Atlag.java
@@ -0,0 +1,22 @@
+package atlag;
+
+import java.util.Scanner;
+
+public class Atlag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	double tomb[] = new double[3];
+	Scanner bemenet = new Scanner(System.in);
+	
+	for (int i = 0; i < tomb.length; i++) {
+		System.out.println("Kerem adja meg a " + (i + 1) + "elem erteket!");
+		tomb[i] = bemenet.nextDouble();
+	}
+		double mertani_atlag = 1;
+	}
+	
+	
+
+}
